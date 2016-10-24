767f301d018ac3d31a0d6f1ac30f2b813bbc3cb5
Øystein Barstad <oebars@gmail.com>
2016-10-24 11:06:50 +0200

    idkwtf

---
+++ ShippernShippingDynamic/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shippern shipping</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                Shippern Shipping
+            </h1>
+            <nav>
+                <ul>
+                    <a href ="index.php">
+                    <li>
+                        Home
+                    </li>
+                    </a>
+                    
+                    <a href="pickUserPage.php">
+                    <li>
+                        Login
+                    </li>
+                    </a>
+                    
+                    <a href="ourFleet.php">
+                    <li>
+                       
+                        Our fleet
+                    </li>
+                    </a>
+                    
+                    <a href="forInvestors.php">
+                    <li>
+                        For investors
+                    </li>
+                    </a>
+                    
+                    <a href="aboutUs.php">
+                    <li>
+                        About us
+                    </li>
+                    </a>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
